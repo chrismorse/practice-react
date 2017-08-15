6bc61b9eb4015ed049ac3936c9974d9e464b60e5
chrismorse <github@beeksma.com>
2017-08-15 08:37:07 -0700

    multiple buttons, parent props

--- proj1/components/App.js
+++ proj1/components/App.js
@@ -1,21 +1,52 @@
 ﻿import React from 'react';
 
-class App extends React.Component {
-	state = { counter: 0 }
+class Button extends React.Component {
   
   handleClick = () => {
-  	this.setState({
-    	counter: this.state.counter + 1
-    })
+      this.props.onClickFunction(this.props.incrementValue);
   };
   
   render() {
     return (
       <button onClick={this.handleClick}>
-        {this.state.counter}
+        +{this.props.incrementValue}
       </button>
     );
   }
 }
 
+const Result = (props) => {
+  return (
+    <div>{props.counter}</div>   //no 'this' since a function component
+  );
+}
+
+class App extends React.Component {
+	state = { counter: 0 }
+  
+  incrementCounter = (incrementValue) => {
+  	this.setState((prevState) => ({
+    	counter: prevState.counter + incrementValue
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Button onClickFunction={this.incrementCounter} incrementValue={1}/>
+        <Button onClickFunction={this.incrementCounter} incrementValue={2}/>
+        <Button onClickFunction={this.incrementCounter} incrementValue={5}/>
+        <Button onClickFunction={this.incrementCounter} incrementValue={10}/>
+        <Button onClickFunction={this.incrementCounter} incrementValue={20}/>
+        <Result counter={this.state.counter}/>
+      </div>
+    );
+
+  }
+
+}
+
+
+
 module.exports = App;
+
